2f8bef8f8c526eaba14a43c65ac918800039b35c
abersheeran <me@abersheeran.com>
2020-01-12 01:46:59 +0800

    change default logging level

--- socks5/server/__main__.py
+++ socks5/server/__main__.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     format="[%(asctime)s] [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 try:
     Socks5(host=sys.argv[1], port=int(sys.argv[2])).run()
